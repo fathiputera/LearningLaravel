1b931aaa5f8b5b48ada99f03ce3285095ef32983
fathiputera <fathiputera@gmail.com>
2021-10-08 11:06:22 +0800

    Custom Pagination
    Dalam video minit ke 1:24:00
    Documentation boleh didapati di : https://laravel-livewire.com/docs/2.x/pagination

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -27,7 +27,7 @@
             <p class="text-gray-800">{{$comment->body}}</p>
         </div>
         @endforeach
-        {{$comments->links()}}
+        {{$comments->links('pagination-links')}}
     </div>
 </div>
 
