e6d3685e5f6fbb22ce7c04f311101aed5c1292cb
fathiputera <fathiputera@gmail.com>
2021-10-08 15:44:26 +0800

    Upload image
    Dalam video minit ke 1:36:00
    Documentation boleh didapati di :
    Emit Event
    https://laravel-livewire.com/docs/2.x/events

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,13 @@
     <title>Livewire</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     @livewireStyles
+    @livewireScripts
 
 </head>
 
 <body>
     {{--  @livewire('comments' , ['initialComments' => $comments]) --}}
     @livewire('comments');
-    @livewireScripts
 </body>
 
 </html>
