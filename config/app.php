2446a5334cacd175540602cb78732228fd5e4cd2
fathiputera <fathiputera@gmail.com>
2021-10-12 16:09:58 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://learninglaravel.tyhmj5iyaq-dv13xzrmv4gq.p.runcloud.link/'),
 
     'asset_url' => env('ASSET_URL', null),
 
